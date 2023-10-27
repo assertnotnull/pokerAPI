fe882b7ffbd7c321a82b26f91b613a8f5b2e6279
Patrice Gauthier <patgauth@gmail.com>
2023-10-26 22:36:28 -0400

    use safeParse instead

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,14 +16,14 @@ export class AppController {
 
   @Post('hands')
   evaluateHand(@Body() body: PokerHandsDto): string {
-    try {
-      const validHands = handSchema.parse(body);
-      return this.appService.evaluateHands(validHands.hands);
-    } catch (err) {
-      throw new HttpException(
-        'Invalid hands, expect hands to be an array of 5 cards: [["TH", "9C", "8D", "7S", "6C"], ["2S", "3S", "4S", "5S", "6S"]]. Where S=♠, H=♥, D=♦, C=♣',
-        HttpStatus.BAD_REQUEST,
-      );
+    const outcome = handSchema.safeParse(body);
+    if (outcome.success) {
+      return this.appService.evaluateHands(outcome.data.hands);
     }
+
+    throw new HttpException(
+      'Invalid hands, expect hands to be an array of 5 cards: [["TH", "9C", "8D", "7S", "6C"], ["2S", "3S", "4S", "5S", "6S"]]. Where S=♠, H=♥, D=♦, C=♣',
+      HttpStatus.BAD_REQUEST,
+    );
   }
 }
