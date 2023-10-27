864151d8a0885a88b4b062afe6eac7df690507e9
Patrice Gauthier <patgauth@gmail.com>
2023-10-26 22:37:22 -0400

    fix test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokerHandsDto } from './pokerhands.dto';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -16,13 +17,13 @@ describe('AppController', () => {
 
   describe('poker hands', () => {
     it('should return "Black wins. - with high card: 9"', () => {
-      const body = {
+      const body: PokerHandsDto = {
         hands: [
           ['2H', '3D', '5S', '9C', 'KD'],
           ['2C', '3H', '4S', '8C', 'AH'],
         ],
       };
-      expect(appController.evaluateHand(body as unknown as Body)).toBe(
+      expect(appController.evaluateHand(body)).toBe(
         AppService.sortCards(body.hands[1]).join(' '),
       );
     });
