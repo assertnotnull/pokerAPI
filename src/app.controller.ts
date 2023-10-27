d17b61331b25828df66b57c6d79dff8180dc7e37
Patrice Gauthier <patgauth@gmail.com>
2023-10-26 22:38:56 -0400

    align routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ import { AppService } from './app.service';
 import { handSchema } from './app.schema';
 import { PokerHandsDto } from './pokerhands.dto';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
